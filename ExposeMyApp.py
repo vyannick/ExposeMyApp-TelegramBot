f05a484bb881648c0c2899089880c30e28c82c36
Yannick V <Yannick V>
2021-07-07 19:35:09 +0200

    added tcp possibility

--- ExposeMyApp.py
+++ ExposeMyApp.py
@@ -17,27 +17,44 @@ ngrok_running = False
 def expose(update, context,services,ngrok):
     global ngrok_running
 
-    if ngrok_running:
-        update.message.reply_text("Ngrok already running service " + str(ngrok_running))
-        return
+    try:
 
-    service = context.args[0]
-    service_port = str(services.get(service))
+        if ngrok_running:
+            update.message.reply_text("Ngrok already running service " + str(ngrok_running))
+            return
 
-    if service_port == None:
-        update.message.reply_text("Unknown service " + str(service))
-        return
+        service = context.args[0]
+        service_object = services.get(service)
+        if service_object is None:
+            logging.info("Unknown service: " + str(service))
+            update.message.reply_text("Unknown service " + str(service))
+            return
+        service_port = service_object.get("port")
+        service_type = service_object.get("type")
 
-    ngrok_start = subprocess.Popen([ngrok,"http",service_port],text=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+        if service_port is None or service_type is None:
+            logging.info("Service: " + str(service) + " not well defined")
+            update.message.reply_text("Service: " + str(service) + " not well defined")
+            return
+
+
+        service_port = str(service_port)
+        service_type = str(service_type)
+
+        logging.info("Running " + service + " on port: " + service_port)
+        ngrok_start = subprocess.Popen([ngrok,service_type,service_port],text=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
     
-    if ngrok_start.poll() != None:
-        update.message.reply_text("Ngrok failed to start app " + str(service) + ":\n " + str(ngrok_start.communicate()))
-        return
+        if ngrok_start.poll() != None:
+            update.message.reply_text("Ngrok failed to start app " + str(service) + ":\n " + str(ngrok_start.communicate()))
+            return
 
-    print("Ngrok started")
-    ngrok_running = service
+        logging.info("Ngrok started")
+        ngrok_running = service
 
-    return_active_ngrok_url(update,service=service)
+        return_active_ngrok_url(update,service=service)
+    except Exception as e:
+        update.message.reply_text("An error occured")
+        logging.error(e)
 
 def return_active_ngrok_url(update,service=None,attempts=0):
     global ngrok_running
@@ -52,7 +69,7 @@ def return_active_ngrok_url(update,service=None,attempts=0):
         if attempts < 3:
             return_active_ngrok_url(update,service=ngrok_running,attempts=attempts+1)
         else:
-            print(e)
+            logging.error(e)
             update.message.reply_text("Couldn't retrieve ngrok URL:\n " + str(e.errno))
             stop_ngrok()
             return
@@ -61,13 +78,13 @@ def return_active_ngrok_url(update,service=None,attempts=0):
         if attempts < 3:
             return_active_ngrok_url(update,service=ngrok_running,attempts=attempts+1)
         else:
-            update.message.reply_text("Couldn't retrieve ngrok URL:\n " + str(ngrok_url_request.status_code))
+            update.message.reply_text("Couldn't retrieve ngrok URL:\n " + str(ngrok_url.stderr))
     else:
         try:
             ngrok_url = ngrok_url_request.json()["tunnels"][0]["public_url"]
         except Error as e:
-            print(str(e))
-            print(ngrok_url_request.json())
+            logging.error(str(e))
+            logging.error(ngrok_url_request.json())
 
             if attempts < 3:
                 return_active_ngrok_url(update,service=ngrok_running,attempts=attempts+1)
@@ -79,8 +96,9 @@ def return_active_ngrok_url(update,service=None,attempts=0):
 
 def stop_ngrok():
     global ngrok_running
-    subprocess.run(["pkill", "ngrok"]) #TODO finegrained stop
+    subprocess.run(["pkill", "ngrok"])
     ngrok_running = False
+    logging.info("Ngrok stopped")
 
 def stop(update,context):
     if ngrok_running:
@@ -89,6 +107,11 @@ def stop(update,context):
         update.message.reply_text("Ngrok stopped")
     else:
         update.message.reply_text("Ngrok not active")
+
+def unknown_command():
+    update.message.reply_text("Unknown command")
+    logging.info("Unknown command received")
+    return
 
 def main():
     if "API_KEY" in os.environ and "SERVICES_FILE" in os.environ and "NGROK_BINARY" in os.environ:
@@ -104,13 +127,15 @@ def main():
         dispatcher = updater.dispatcher
         dispatcher.add_handler(telegram.ext.CommandHandler('expose',lambda bot,update : expose(bot,update,services,ngrok)))
         dispatcher.add_handler(telegram.ext.CommandHandler('stop',stop))
+        dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.all, unknown_command))
         updater.start_polling()
         updater.idle()
     else:
-        print("No API_KEY and/or SERVICES_FILE and/or NGROK_BINARY in environment")
+        logging.error("No API_KEY and/or SERVICES_FILE and/or NGROK_BINARY in environment")
         sys.exit(0)
 
 if __name__ == '__main__':
     main()
+
 
 
